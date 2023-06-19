ba5bc61c456ebe9bbd15f5560659786cb39f5630
shubhampathak <shubham.pathak926@gmail.com>
2023-06-19 14:33:16 +0530

    modal and routes for post

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import ConnectDb from "./mongoDB/connect.js";
 
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dall-ERoutes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E");
 });
