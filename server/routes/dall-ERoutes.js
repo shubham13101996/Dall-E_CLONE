1aef91e0835451350e27fc8d57ec42855f511e60
shubhampathak <shubham.pathak926@gmail.com>
2023-06-19 15:50:02 +0530

    genrating images by giving prompt

--- server/routes/dall-ERoutes.js
+++ server/routes/dall-ERoutes.js
@@ -6,4 +6,35 @@ dotenv.config();
 
 const router = express.Router();
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openAi = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.send("Hello from dalle");
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const aiResponse = await openAi.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    // console.log(aiResponse.data.data[0]);
+    const image = aiResponse.data.data[0].b64_json;
+
+    // console.log(image);
+    res.status(200).json({
+      photo: image,
+    });
+  } catch (error) {
+    // console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
 export default router;
